dfeea2dbaf37fbb4d32a4bed49a456b20452eeaf
Bahram Maravandi <bahi@hey.com>
2022-05-12 06:53:36 +0000

    config updates logging, reloading

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return "Hello, World! Namaste"
+   return "Hello, World!"
 
 # Default app port
 app_port = 8000
--- app/gunicorn_conf.py
+++ app/gunicorn_conf.py
@@ -4,7 +4,7 @@ from multiprocessing import cpu_count
 import os
 
 # Default app port
-app_port = 5000
+app_port = 8000
 
 if os.environ.get('APP_PORT') is not None:
     app_port = os.environ['APP_PORT']
@@ -14,3 +14,11 @@ bind = '0.0.0.0:' + str(app_port)
 
 # Worker Options
 workers = (2* cpu_count()) + 1
+
+# Reload
+reload_engine = 'inotify'
+
+# Logging
+accesslog = '../logs/app_access.log'
+errorlog = '../logs/app_error.log'
+loglevel = 'debug'
